848b3c38f43bad2687d8a5bad259c05a675a0c19
rody7val <rod7val@gmail.com>
2017-06-12 02:27:20 -0300

    Añadida función getApiToken()

--- DeepSleepDS18B20.ino
+++ DeepSleepDS18B20.ino
@@ -1,56 +1,121 @@
 // ESP8266 core
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
 #include <WiFiManager.h>
 
+// JSON encoder/decoder
+#include <ArduinoJson.h>
+DynamicJsonBuffer jsonBuffer;
+
 // Depuración remota sobre telnet
-#include "RemoteDebug.h"
+#include <RemoteDebug.h>
 RemoteDebug Debug;
 
 // Dependencias para DS18B20
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-// Configuraciones
-long startMills;
-const char* ID = "A";
 #define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void setup(void){
+// Init
+long startMills;
+float t = 0;
+int vcc;
+bool logged = false;
+const String _id = "A";
 
+// .env
+#include "config.h"
+
+void setup(void){
   // Inicializar el tiempo de inicio del programa en milisegundos
   startMills = millis();
 
   // Conectar a la red local WiFi
   WiFiManager wifi;
-  wifi.autoConnect("rod7val", "77952080");
+  if (!wifi.autoConnect(wifi_ssid, wifi_pass)) {
+    Debug.println("No se puede conectar!");
+  }
 
   // Inicializar depuración
-  Debug.begin(ID);
+  Debug.begin(_id);
   Debug.setResetCmdEnabled(true);
   
   // Si estamos aquí, estamos conectado a la red wifi!!
   Debug.print("Conectado a WiFi! La IP es: ");
   Debug.println(WiFi.localIP());
-
+  
   // Inicializar libreria DallasTemperature
   sensors.begin();
-
 }
 
-void loop(void){
 
+void loop(void){
   // Manejo de depurción
   Debug.handle();
+  Debug.print("Tiempo -> ");
   Debug.println(millis() - startMills);
 
-  // Obtener y mostrar temperatura
-  sensors.requestTemperatures();
-  Debug.print("Temperatura -> ");
-  Debug.println(sensors.getTempCByIndex(0));
+  // Obtener Voltaje del ESP8266
+  vcc = ESP.getVcc();
+  Debug.print("Vcc -> ");
+  Debug.println(vcc);
 
+  // Obtener Temperatura
+  sensors.requestTemperatures();
+  t = sensors.getTempCByIndex(0);
+  Debug.print("Temperatura -> ");
+  Debug.println(t);
+
+  // Obtener Token
+  if (!logged) {
+    getApiToken(api_user_email, api_user_pass);
+  }
+
+  Debug.print("Token -> ");
+  Debug.println(api_user_token);
+  
+  // Proximo paso, enviar datos a la API
+  // sendData();
+}
+
+
+void getApiToken(String email, String pass) {
+  // Instancia cliente
+  HTTPClient http;
+
+  // Request
+  http.begin("http://"+ api_host +":"+ api_port + api_login);
+  http.addHeader("Content-Type", "application/json");
+
+  // Enviar json! String con comillas escapadas:
+  int httpCode = http.POST("{\"email\": \"" + email + "\",\"password\": \"" + pass + "\"}");
+
+  // Response
+  if (httpCode == 200) {
+
+    // Obtener y decodificar JSON
+    String json = http.getString();
+    JsonObject& user = jsonBuffer.parseObject(json);
+
+    // Si falla
+    if (!user.success()) {
+      Debug.printf("Falló la decodificación. Error nº ", http.errorToString(httpCode).c_str());
+      logged = false;   // ??
+      http.end();
+      return;
+    }
+
+    // Token!
+    api_user_token = user["token"];
+    logged = true;
+    Debug.println(api_user_token);
+    
+  } else {
+    Debug.printf("Error nº ", http.errorToString(httpCode).c_str());
+    logged = false;   // Server caído?
+  }
+  
+  http.end();
 }
---
+++ config.h
@@ -0,0 +1,13 @@
+// Red local wifi
+const char * wifi_ssid = "YOUR_LOCAL_SSID";
+const char * wifi_pass = "YOUR_LOCAL_PASS";
+
+// Node.js api server
+const String api_host = "YOUR_API_HOST";
+const String api_port = "YOUR_API_PORT";
+const String api_login = "/path/to/your/api/login";
+
+// user api:
+const char * api_user_email = "YOUR_API@EMAIL.COM";
+const char * api_user_pass = "YOUR_API_PASSWORD";
+const char * api_user_token;
