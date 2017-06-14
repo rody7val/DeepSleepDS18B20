d4088fd8ecd95708ddd1807b9919be0328ccc2ea
rody7val <rod7val@gmail.com>
2017-06-14 18:27:36 -0300

    Añadida función promesa() para enviar petición HTTP

--- config.h
+++ config.h
@@ -6,8 +6,19 @@ const char * wifi_pass = "YOUR_LOCAL_PASS";
 const String api_host = "YOUR_API_HOST";
 const String api_port = "YOUR_API_PORT";
 const String api_login = "/path/to/your/api/login";
+const String api_new_sensor = "/path/to/your/api/sensor";
 
 // user api:
-const char * api_user_email = "YOUR_API@EMAIL.COM";
-const char * api_user_pass = "YOUR_API_PASSWORD";
-const char * api_user_token;
+const String api_user_email = "YOUR_API@EMAIL.COM";
+const String api_user_pass = "YOUR_API_PASSWORD";
+
+// dinamic
+static const char * api_user_token;
+static const char * sensor_saved_id;
+static String response;
+
+
+
+
+
+
